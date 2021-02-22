0b0df531a20a0d89b0400c796306d09203f24b89
Alberto Fabian <lf_alberto@hotmail.com>
2021-02-22 17:25:57 -0600

    Actalizaciones comisa

--- app/componentes/scripts.php
+++ app/componentes/scripts.php
@@ -6,7 +6,7 @@
 <script src="<?php echo HTTP_HOST . 'app/modulos/productos/productos.js' ?>"></script>
 <script src="<?php echo HTTP_HOST . 'app/modulos/cajas/cajas.js' ?>"></script>
 <script src="<?php echo HTTP_HOST . 'app/modulos/traspasos/traspasos.js' ?>"></script>
-<script src="<?php echo HTTP_HOST . 'app/modulos/pos/pos.js' ?>"></script>
+<!-- <script src="<?php// echo HTTP_HOST . 'app/modulos/pos/pos.js' ?>"></script> -->
 <script src="<?php echo HTTP_HOST . 'app/modulos/gastos/gastos.js' ?>"></script>
 <script src="<?php echo HTTP_HOST . 'app/modulos/compras/compras.js' ?>"></script>
 <script src="<?php echo HTTP_HOST . 'app/modulos/ingresos/ingresos.js' ?>"></script>
