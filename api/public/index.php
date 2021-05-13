0a12ca75f2b1861b82a1b42fe8b237c1899eea00
Alberto Fabian <lf_alberto@hotmail.com>
2021-05-13 13:52:09 -0500

    api

--- api/public/index.php
+++ api/public/index.php
@@ -50,4 +50,44 @@ $app->get('/prueba', function (Request $request, Response $response) {
 $app->post('/login', function (Request $request, Response $response) {
     echo "Aqui toy";
 });
+
+$app->get('/traspaso/{id}', function (Request $request, Response $response, array $args) {
+    $id = "T-".$args['id'];
+    try {
+        //code...
+        $sql = "SELECT tps.*,usr_reg.usr_nombre AS registro,
+        ams_ori.ams_nombre AS origen,
+        usr_rec.usr_nombre AS receptor,
+        ams_des.ams_nombre AS destino FROM tbl_traspasos_tps tps 
+        JOIN tbl_usuarios_usr usr_reg ON usr_reg.usr_id =tps.tps_user_registro 
+        JOIN tbl_almacenes_ams ams_ori ON ams_ori.ams_id=tps.tps_ams_id_origen 
+        JOIN tbl_usuarios_usr usr_rec ON usr_rec.usr_id =tps.tps_user_id_receptor 
+        JOIN tbl_almacenes_ams ams_des ON ams_des.ams_id=tps.tps_ams_id_destino 
+        WHERE tps_num_traspaso=? LIMIT 1 ";
+        $con = Conexion::conectar();
+        $pps = $con->prepare($sql);
+        $pps->bindValue(1, $id);
+        $pps->execute();
+        $rs = $pps->fetch();
+        $listp = json_decode($rs["tps_lista_productos"], true);
+        $informacionQR = array(
+            'productos'=>$listp
+    ,
+            
+            'infvendedor' => array(
+                'idusr' => $rs["tps_user_id_receptor"],
+                'nombre' => $rs["receptor"],
+                'camioneta' => $rs["destino"],
+            )
+        );
+        echo json_encode($informacionQR,true);
+    } catch (PDOException $th) {
+        //throw $th;
+        return false;
+    } finally {
+        $pps = null;
+        $con = null;
+    }
+});
+
 $app->run();
