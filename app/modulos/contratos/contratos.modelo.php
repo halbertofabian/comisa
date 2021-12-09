fad531021e43f0823c0e88c8a6f25e161928364e
Your Name <victor081196>
2021-12-09 10:30:53 -0600

    ventas

--- app/modulos/contratos/contratos.modelo.php
+++ app/modulos/contratos/contratos.modelo.php
@@ -1116,7 +1116,7 @@ class ContratosModelo
     {
         try {
             //code...
-            if ($filtro == 0) {
+            if ($filtro == '0') {
                 $sql = "SELECT ctr_id,ctr_numero_cuenta,ctr_ruta,ctr_cliente,clts_domicilio,clts_col,clts_curp,clts_telefono FROM tbl_contrato_crt_1 WHERE ctr_id_vendedor = ? ORDER BY ctr_id ASC";
                 $con = Conexion::conectar();
                 $pps = $con->prepare($sql);
@@ -1124,7 +1124,7 @@ class ContratosModelo
                 $pps->execute();
                 return $pps->fetchAll();
             } else {
-                $sql = "SELECT ctr_id,ctr_numero_cuenta,ctr_ruta,ctr_cliente,clts_domicilio,clts_col,clts_curp,clts_telefono FROM tbl_contrato_crt_1 WHERE ctr_cliente LIKE '%$filtro%' OR clts_curp LIKE '%$filtro%' AND ctr_id_vendedor = ? ORDER BY ctr_id ASC";
+                $sql = "SELECT ctr_id,ctr_numero_cuenta,ctr_ruta,ctr_cliente,clts_domicilio,clts_col,clts_curp,clts_telefono FROM tbl_contrato_crt_1 WHERE ctr_id_vendedor = ? AND ctr_cliente LIKE '%$filtro%' OR clts_curp LIKE '%$filtro%' ORDER BY ctr_id ASC";
                 $con = Conexion::conectar();
                 $pps = $con->prepare($sql);
                 $pps->bindValue(1, $ctr_id_vendedor);
