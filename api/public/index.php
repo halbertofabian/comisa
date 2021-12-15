64dfe1bf3341f77de2a0c89ea4259e6d6cfe1831
Alberto Fabian <lf_alberto@hotmail.com>
2021-12-14 20:29:41 -0600

    ok

--- api/public/index.php
+++ api/public/index.php
@@ -317,6 +317,7 @@ $app->get('/obtener_ventas/{ctr_id_vendedor}/{filtro}', function (Request $reque
 
     return json_encode($getAllCtra, true);
 });
+
 $app->get('/obtener_pendientes/{ctr_id_vendedor}', function (Request $request, Response $response, array $args) {
     $ctr_id_vendedor =  $args['ctr_id_vendedor'];
 
@@ -324,6 +325,8 @@ $app->get('/obtener_pendientes/{ctr_id_vendedor}', function (Request $request, R
 
     return json_encode($getAllCtra, true);
 });
+
+
 
 //API'S PARA LA APP DE COMISA COBRNZA
 
