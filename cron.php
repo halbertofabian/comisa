1462fd028669879efd90d729e07883fe09901637
Alberto Fabian <lf_alberto@hotmail.com>
2021-12-20 17:15:54 -0600

    ok

--- cron.php
+++ cron.php
@@ -1,11 +1,6 @@
 <?php
-
-
+// wget -O /dev/null https://pruebas-comisa.softmor.com/api/public/prueba_cron
 include_once 'config.php';
 
-preArray(DOCUMENT_ROOT);
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, HTTP_HOST . 'api/public/prueba_cron');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$res = curl_exec($ch);
-curl_close($ch);
+
+
