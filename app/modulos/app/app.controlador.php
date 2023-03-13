c428b4803b948680d474e8bbcf8ba61af47b4d14
Alberto Fabian <lf_alberto@hotmail.com>
2023-03-13 13:10:32 -0600

    ok

--- app/modulos/app/app.controlador.php
+++ app/modulos/app/app.controlador.php
@@ -1584,18 +1584,18 @@ class AppControlador
         return array(
             '1' => array(
                 'scl_id' => 1,
-                'scl_nombre' => 'TUXTEPEC',
+                'scl_nombre' => 'COMISA TUXTEPEC OAXACA',
                 'scl_url' => 'https://tuxtepec-comisa.softmor.com/'
             ),
             '2' => array(
                 'scl_id' => 2,
 
-                'scl_nombre' => 'TIERRA BLANCA',
+                'scl_nombre' => 'COMISA TIERRA BLANCA',
                 'scl_url' => 'https://tierrablanca-comisa.softmor.com/'
             ),
             '3' => array(
                 'scl_id' => 3,
-                'scl_nombre' => 'COSAMALOAPAN',
+                'scl_nombre' => 'COMISA COSAMALOAPAN',
                 'scl_url' => 'https://cosamaloapan-comisa.softmor.com/'
             ),
             '4' => array(
