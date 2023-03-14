5fc2fbca4a8c3bf9c969cb0f282191b3f454fe1e
Alberto Fabian <lf_alberto@hotmail.com>
2023-03-14 10:17:07 -0600

    ok

--- api/public/index.php
+++ api/public/index.php
@@ -391,7 +391,7 @@ $app->post('/subir_contratos_new', function (Request $request, Response $respons
 });
 
 
-$app->post('/comisa-datos-cobranza', function (Request $request, Response $response) {
+$app->post('/-', function (Request $request, Response $response) {
     $json = $request->getBody();
 
     $data = json_decode($json, true);
@@ -412,7 +412,7 @@ $app->post('/comisa-datos-cobranza', function (Request $request, Response $respo
     # code...
 
 });
-$app->post('/-', function (Request $request, Response $response) {
+$app->post('/comisa-datos-cobranza', function (Request $request, Response $response) {
     $json = $request->getBody();
     $datosVendedor = json_decode($json, true);
     try {
